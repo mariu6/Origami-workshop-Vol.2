6974ebc7b9d2689cbb92a5325a99bdacf9364970
Mariusz Ilijew <mariu6@gmail.com>
2020-10-24 23:36:54 +0200

    feat: implement header component

---
+++ src/components/header/index.js
@@ -0,0 +1,20 @@
+import styles from './index.module.css';
+import Link from "../link"
+
+const Header = () => {
+    return (
+        <header className={styles.navigation}>
+            <ul>
+                <li>picture?</li>
+                <Link title="Going to 1" href="#"/>
+                <Link title="Going to 2" href="#"/>
+                <Link title="Going to 3" href="#"/>
+                <Link title="Going to 4" href="#"/>
+                <Link title="Going to 5" href="#"/>
+                <Link title="Going to 6" href="#"/>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
