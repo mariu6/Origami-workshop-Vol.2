fe40c9db5ac6328df40a61f86836ea8fc5584d5c
Mariusz Ilijew <mariu6@gmail.com>
2020-11-02 15:05:57 +0100

    feat: styled components

--- src/components/submit-button/index.js
+++ src/components/submit-button/index.js
@@ -1,9 +1,28 @@
-import styles from './index.module.css';
+// import styles from './index.module.css';
+import styled from "styled-components";
 
-const SubmitButton = ({ title }) => {     
+const SubmitButton = ({ title }) => {
   return (
-      <button className={styles.submit}>{title}</button>            
+    <Button >{title}</Button>
   )
 }
+
+const Button = styled.button`
+  background-color: #234465;
+  color: white;
+  padding: 1%;
+  width: 7%;
+  border-radius: 6px;
+  display: block;
+  margin: 0.5% auto 2% auto;
+  border: 2px solid white;
+
+  &:hover {
+     background-color: white;
+    border: 2px solid #234465;
+    color: #234465;
+    font-style: italic;
+}
+`
 
 export default SubmitButton;
--- src/pages/share-thoughts/index.js
+++ src/pages/share-thoughts/index.js
@@ -2,19 +2,35 @@ import Origamis from "../../components/origamis";
 import PageLayout from "../../components/page-layout";
 import SubmitButton from "../../components/submit-button";
 import Title from "../../components/title";
-import styles from './index.module.css';
+// import styles from './index.module.css';
+import styled from "styled-components";
 
 const App = () => {
   return (
     <PageLayout>
-       <Title title="Share your thoughts..." />
-        <textarea className={styles.textarea} placeholder="Publication..." />
-        <SubmitButton title="Post" />
-        <div>
-          <Origamis length={3} />
-        </div>
+      <Title title="Share your thoughts..." />
+      <TextArea placeholder="Publication..." />
+      <RedSubmitButton title="Post" />
+      <div>
+        <Origamis length={3} />
+      </div>
     </PageLayout>
   );
 }
+
+// Styled Components (за css)component:
+const TextArea = styled.textarea`
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 300px;
+    height: 200px;
+    resize: none;
+`
+
+const RedSubmitButton = styled(SubmitButton)`
+position: relative;
+`
+
 
 export default App;
