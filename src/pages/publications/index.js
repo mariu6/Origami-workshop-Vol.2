209b29b9d934a92cd4f8f4f42f23f7297cc1dc60
Mariusz Ilijew <mariu6@gmail.com>
2020-11-30 21:12:15 +0100

    fix: more classes transferred to hooks functions

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -1,21 +1,29 @@
 import PageLayout from "../../components/page-layout";
 import Origamis from "../../components/origamis";
 import Title from "../../components/title";
-import UserContext from "../../Context";
-import { Component } from "react";
+// import { Component } from "react";
 
-class Publications extends Component {
-    static contextType = UserContext;   // за да можем да достъпим каквото сме подали в контекста (в случая - user и _id)
-
-    render() {
-        console.log(`publications`,this.context);
-        return (
-            <PageLayout>
-                <Title title="Publications" />
-                <Origamis />
-            </PageLayout>
-        )
-    }
+const PublicationsPage = () => {
+    return (
+        <PageLayout>
+            <Title title="Publications" />
+            <Origamis />
+        </PageLayout>
+    )
 }
 
-export default Publications;                 // Аз обаче ще го оставя Арр, за да се знае какъв е бил оригинално файла, преди да го преработим
+// class Publications extends Component {
+//     static contextType = UserContext;   // за да можем да достъпим каквото сме подали в контекста (в случая - user и _id)
+
+//     render() {
+//         console.log(`publications`,this.context);
+//         return (
+//             <PageLayout>
+//                 <Title title="Publications" />
+//                 <Origamis />
+//             </PageLayout>
+//         )
+//     }
+// }
+
+export default PublicationsPage;
